6b548ec8546936021fd0792159794f14e043d554
Stuart Garner <sgarner4@networkrail.co.uk>
2023-05-08 09:45:14 +0100

    Add animation to Popup component using Framer Motion library

--- components/popup/popup.tsx
+++ components/popup/popup.tsx
@@ -1,17 +1,34 @@
+import { motion } from "framer-motion";
+
 type PropType = {
   children: React.ReactNode;
 };
 
 const Popup = ({ children }: PropType) => {
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{
+        repeat: 0,
+        duration: 0.5,
+      }}
       id="contact-form"
       className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex justify-center items-center"
     >
-      <div className="py-10 px-20 bg-white shadow-xl rounded-lg">
+      <motion.div
+        initial={{ opacity: 0, y: 25 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          repeat: 0,
+          duration: 0.5,
+          delay: 0.5,
+        }}
+        className="py-10 px-20 bg-white shadow-xl rounded-lg"
+      >
         {children}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
